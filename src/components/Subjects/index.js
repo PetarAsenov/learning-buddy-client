166eed264bb5d6257fd390cf9083119e0d9d064c
PetarAsenov <p.a.asenov@gmail.com>
2020-04-24 11:28:38 +0200

    add welcome message

--- src/components/Subjects/index.js
+++ src/components/Subjects/index.js
@@ -3,7 +3,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchSubjects } from "../../store/subject/actions";
 import { selectSubject } from "../../store/subject/selectors";
 import Subject from "../Subject";
-import { Col, Row } from "react-bootstrap";
 
 export default function ListSubjects() {
   const dispatch = useDispatch();
@@ -14,14 +13,5 @@ export default function ListSubjects() {
 
   const subjects = useSelector(selectSubject);
 
-  return (
-    <Row>
-      <Col md={{ span: 5, offset: 0 }}>
-        <h1>Welcome</h1>
-      </Col>
-      <Col md={{ span: 5, offset: 5 }}>
-        <Subject subjects={subjects} />
-      </Col>
-    </Row>
-  );
+  return <Subject subjects={subjects} />;
 }
--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -1,12 +1,32 @@
-import React from 'react'
-import ListSubjects from '../../components/Subjects'
-import ListTeachers from '../../components/ListTeachers'
+import React from "react";
+import ListSubjects from "../../components/Subjects";
+import ListTeachers from "../../components/ListTeachers";
+import { Col, Row } from "react-bootstrap";
+import "./styles.css";
 
 export default function index() {
   return (
     <div>
-      <ListSubjects />
+      <Row style={{ dispaly: "flex" }}>
+        <Col className="welcome" md={{ span: 5, offset: 1 }}>
+          <h2>
+            Welcome to the Learning Buddy platform
+            <br />
+            <br />
+            Here you can learn and help others learn
+          </h2>
+          <br />
+          <p>
+            Register for an upcoming session to experience
+            <br /> the personal and interactive learning
+            <br /> with one of our volunteering teachers{" "}
+          </p>
+        </Col>
+        <Col md={{ span: 5, offset: 0 }}>
+          <ListSubjects />
+        </Col>
+      </Row>
       <ListTeachers />
     </div>
-  )
+  );
 }
