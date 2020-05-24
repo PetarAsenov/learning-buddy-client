7e3d7a2fe82fffa9c35cb683e775d9b271344316
PetarAsenov <p.a.asenov@gmail.com>
2020-05-24 15:10:00 +0200

    format login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -34,12 +34,15 @@ export default function SignUp() {
   return (
     <Container>
       <Form as={Col} md={{ span: 6, offset: 3 }} className="mt-5">
-        <h1 className="mt-5 mb-5">Login</h1>
+        <Form.Group className="mt-5 mb-5 text-center">
+        <h1>Login</h1>
+        </Form.Group>
+
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control
             value={email}
-            onChange={event => setEmail(event.target.value)}
+            onChange={(event) => setEmail(event.target.value)}
             type="email"
             placeholder="Enter email"
             required
@@ -50,19 +53,19 @@ export default function SignUp() {
           <Form.Label>Password</Form.Label>
           <Form.Control
             value={password}
-            onChange={event => setPassword(event.target.value)}
+            onChange={(event) => setPassword(event.target.value)}
             type="password"
             placeholder="Password"
             required
           />
         </Form.Group>
-        <Form.Group className="mt-5">
-          <Button variant="primary" type="submit" onClick={submitForm}>
+        <Form.Group className="mt-5 text-center">
+          <Button variant="primary" type="submit" onClick={submitForm} style={{width: 100}}>
             Log in
           </Button>
         </Form.Group>
         <Link to="/signup" style={{ textAlign: "center" }}>
-          Click here to sign up
+          Don't have an account yet? Click here to sign up.
         </Link>
       </Form>
     </Container>
